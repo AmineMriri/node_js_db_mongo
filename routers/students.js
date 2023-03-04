a3f840f1448a085b8786532b23b8d774c4b9ec31
Cyclone911 <aminemriri0@gmail.com>
2023-03-04 15:16:16 +0100

    v2

--- routers/students.js
+++ routers/students.js
@@ -7,5 +7,16 @@ router.post('/',async(req,res)=>{
     res.status(201).send(student);
 })
 
+router.get('/',async(req,res)=>{
+    let students = await Student.find();
+    res.status(201).send(students);
+})
+router.get('/id/:id',async(req,res)=>{
+    let student = await Student.findById(req.params.id);
+    if (!student)
+        return res.status(404).send('Student is not found')
+    res.status(201).send(student);
+})
+
 
 module.exports=router;
